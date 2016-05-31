c54c76aaef625132e75986818ca04dc730642306
Marco Balduini <balduini.marco@gmail.com>
2016-05-31 18:00:52 +0200

    - alignement to the new API

--- src/main/java/it/polimi/deib/rsp_service4csparql_client_example/Client_Server_triplewave.java
+++ src/main/java/it/polimi/deib/rsp_service4csparql_client_example/Client_Server_triplewave.java
@@ -2,24 +2,16 @@ package it.polimi.deib.rsp_service4csparql_client_example;
 
 import it.polimi.deib.csparql_rest_api.RSP_services_csparql_API;
 import it.polimi.deib.csparql_rest_api.exception.ServerErrorException;
-import it.polimi.deib.rsp_service4csparql_client_example.results_manipulator.ResultsManipulator;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.log4j.PropertyConfigurator;
-import org.restlet.Application;
-import org.restlet.Component;
-import org.restlet.Restlet;
-import org.restlet.data.Protocol;
-import org.restlet.routing.Router;
-import org.restlet.routing.Template;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.URL;
 import java.util.Hashtable;
 
-public class Client_Server_triplewave extends Application{
+public class Client_Server_triplewave{
 
-    private static Component component;
     private static Hashtable<String, String> queryProxyIdTable = new Hashtable<String, String>();
     private static Logger logger = LoggerFactory.getLogger(Client_Server_triplewave.class.getName());
 
@@ -32,63 +24,74 @@ public class Client_Server_triplewave extends Application{
         boolean startExample = true;
 
         String propertiesFilePath;
+        String log4jConf;
 
-        try{
-            PropertyConfigurator.configure(new URL("http://streamreasoning.org/configuration_files/rspCsparql/log4j.properties"));
-        } catch(Exception e){
-            PropertyConfigurator.configure("config/log4j.properties");
-        }
-
-        if(args.length > 0){
+        if(args.length >0 && args[0] != null)
             propertiesFilePath = args[0];
-        } else {
-            propertiesFilePath = "config/config.properties";
-        }
+        else
+            propertiesFilePath = Client_Server_triplewave.class.getResource( "/config.properties" ).getFile();
+        if(args.length >0 && args[1] != null)
+            log4jConf = args[1];
+        else
+            log4jConf = Client_Server_triplewave.class.getResource( "/log4j.properties" ).getFile();
+
+        if(log4jConf.startsWith("http://"))
+            PropertyConfigurator.configure(new URL(log4jConf));
+        else
+            PropertyConfigurator.configure(log4jConf);
 
         PropertiesConfiguration config = new PropertiesConfiguration(propertiesFilePath);
 
         String key = config.getString("rsp.client.example");
         String csparqlServerAddress = config.getString("rsp.server.address");
 
-        String actual_client_address;
-        int actual_client_port;
-
-        component = new Component();
-        component.getServers().add(Protocol.HTTP, config.getInt("rsp.client.port"));
-        component.getClients().add(Protocol.FILE);
-
-        Client_Server_triplewave csparqlServer = new Client_Server_triplewave();
-        component.getDefaultHost().attach("", csparqlServer);
-
-        component.start();
-
-        actual_client_address = component.getServers().get(0).getAddress();
-        if(actual_client_address == null)
-            actual_client_address = "http://localhost";
-        actual_client_port = component.getServers().get(0).getActualPort();
-
         String query;
 
         String inputstreamName = null;
 
-        String queryURI;
-
         RSP_services_csparql_API csparqlAPI = new RSP_services_csparql_API(csparqlServerAddress);
+
+        //http://localhost:8114/TripleWave-transform/sgraph
+        //http://localhost:8114/TripleWave-replay/sgraph
 
         switch(key){
 
-            case "QUERY1":
+            case "TEST":
                 try {
 
-//				inputstreamName = generalIRI + "onlinefeeding";
+                    csparqlAPI.registerStream("str1", "http://localhost:8114/TripleWave-transform/sgraph");
 
-                    csparqlAPI.registerStream("http://131.175.141.249/TripleWave-transform/sgraph");
+                    query = "REGISTER QUERY q1 AS " +
+                            "PREFIX prov:<http://www.w3.org/ns/prov#> " +
+                            "PREFIX sc:<https://schema.org/> " +
+                            "SELECT ?agent (COUNT(?t) AS ?c) " +
+                            "FROM STREAM <str1> [RANGE 15m STEP 1m] " +
+                            "WHERE { " +
+                            "?t sc:agent ?agent " +
+                            "} " +
+                            "GROUP BY ?agent " +
+                            "ORDER BY desc(?c) " +
+                            "LIMIT 15";
+
+                    csparqlAPI.registerQuery("q1", query);
+                    csparqlAPI.addObserver("q1", "localhost", 8081);
+
+                } catch (ServerErrorException e) {
+                    logger.error("rsp_server4csparql_server error", e);
+                }
+
+                break;
+
+            case "TOPPAGE":
+                try {
+
+                    csparqlAPI.registerStream("wikiTransormationStream", "http://131.175.141.249/TripleWave-transform/sgraph");
 
                     query = "REGISTER QUERY topUpdateWiki AS " +
                             "PREFIX prov:<http://www.w3.org/ns/prov#> " +
                             "PREFIX sc:<https://schema.org/> " +
                             "SELECT ?obj (COUNT(?t) AS ?c) " +
-                            "FROM STREAM <http://131.175.141.249/TripleWave-transform/sgraph> [RANGE 1m STEP 1m] " +
+                            "FROM STREAM <wikiTransormationStream> [RANGE 1m STEP 1m] " +
                             "WHERE { " +
                             "?t sc:object ?obj " +
                             "FILTER (!contains(str(?obj), \"Special:Log\")) " +
@@ -97,8 +100,9 @@ public class Client_Server_triplewave extends Application{
                             "ORDER BY desc(?c) " +
                             "LIMIT 15";
 
-                    queryURI = csparqlAPI.registerQuery("topUpdateWiki", query);
-                    csparqlAPI.addObserver(queryURI, actual_client_address + ":" + actual_client_port + "/results");
+                    csparqlAPI.registerQuery("topUpdateWiki", query);
+                    csparqlAPI.addObserver("topUpdateWiki", "localhost", 8081);
+
 
                 } catch (ServerErrorException e) {
                     logger.error("rsp_server4csparql_server error", e);
@@ -106,16 +110,16 @@ public class Client_Server_triplewave extends Application{
 
                 break;
 
-            case "QUERY2":
+            case "TOPAGENT":
                 try {
 
-                    csparqlAPI.registerStream("http://131.175.141.249/TripleWave-transform/sgraph");
+                    csparqlAPI.registerStream("wikiTransormationStream", "http://131.175.141.249/TripleWave-transform/sgraph");
 
                     query = "REGISTER QUERY topUserWiki AS " +
                             "PREFIX prov:<http://www.w3.org/ns/prov#> " +
                             "PREFIX sc:<https://schema.org/> " +
                             "SELECT ?agent (COUNT(?t) AS ?c) " +
-                            "FROM STREAM <http://131.175.141.249/TripleWave-transform/sgraph> [RANGE 1m STEP 1m] " +
+                            "FROM STREAM <wikiTransormationStream> [RANGE 1m STEP 1m] " +
                             "WHERE { " +
                             "?t sc:agent ?agent " +
                             "} " +
@@ -123,8 +127,36 @@ public class Client_Server_triplewave extends Application{
                             "ORDER BY desc(?c) " +
                             "LIMIT 15";
 
-                    queryURI = csparqlAPI.registerQuery("topUserWiki", query);
-                    csparqlAPI.addObserver(queryURI, actual_client_address + ":" + actual_client_port + "/results");
+                    csparqlAPI.registerQuery("topUserWiki", query);
+                    csparqlAPI.addObserver("topUserWiki", "localhost", 8081);
+
+                } catch (ServerErrorException e) {
+                    logger.error("rsp_server4csparql_server error", e);
+                }
+
+                break;
+
+            case "AVGSENSOR":
+                try {
+
+                    csparqlAPI.registerStream("ssnSensor", "http://localhost:8114/TripleWave-replay/sgraph");
+
+                    query = "REGISTER QUERY averageSensor AS  " +
+                            "PREFIX om-owl:<http://knoesis.wright.edu/ssw/ont/sensor-observation.owl#>  " +
+                            "PREFIX weather: <http://knoesis.wright.edu/ssw/ont/weather.owl#>  " +
+                            "SELECT ?obsProp (AVG(?value) AS ?v)  " +
+                            "FROM STREAM <ssnSensor> [RANGE 15m STEP 1m]  " +
+                            "WHERE {  " +
+                            "?obs om-owl:observedProperty ?obsProp ; " +
+                            "om-owl:result ?res .  " +
+                            "?res om-owl:floatValue ?value  " +
+                            "} " +
+                            "GROUP BY ?obsProp " +
+                            "ORDER BY desc(?v) " +
+                            "LIMIT 15";
+
+                    csparqlAPI.registerQuery("averageSensor", query);
+                    csparqlAPI.addObserver("averageSensor", "localhost", 8081);
 
                 } catch (ServerErrorException e) {
                     logger.error("rsp_server4csparql_server error", e);
@@ -146,21 +178,4 @@ public class Client_Server_triplewave extends Application{
         csparqlAPI.unregisterStream(inputstreamName);
 
     }
-
-    public Restlet createInboundRoot(){
-
-        getContext().getAttributes().put("queryProxyIdTable", Client_Server_triplewave.queryProxyIdTable);
-
-        Router router = new Router(getContext());
-        router.setDefaultMatchingMode(Template.MODE_EQUALS);
-
-        router.attach("/results", ResultsManipulator.class);
-
-        return router;
-    }
-
-    public static int nextID() {
-        return ID++;
-    }
-
 }
